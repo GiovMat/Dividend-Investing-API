a1b7ffc7c8974964b71ac77286683feebca0d04c
GiovMat <giovmat@gmail.com>
2020-06-14 10:34:45 +0200

    -

--- dividend_eng.py
+++ dividend_eng.py
@@ -11,14 +11,14 @@ class Stock():
         self.industry = ticker.asset_profile[symbol]['industry']
         self.sector = ticker.asset_profile[symbol]['sector']
         self.current_price = ticker.financial_data[symbol]['currentPrice']
-        self.dividend_rate = ticker.summary_detail[symbol]['dividendRate'] #! se non ci sono dividendi da errore
+        self.dividend_rate = ''#ticker.summary_detail[symbol]['dividendRate'] #! se non ci sono dividendi da errore
         self.eps = ticker.key_stats[symbol]['trailingEps']
         self.core_growth=''
         self.roe = ''
 
         print(ticker.esg_scores)
 
-stock = Stock('aapl')
+stock = Stock('fbk.mi')
 print(stock.name, stock.symbol, stock.exchange, stock.currency, stock.industry, stock.sector, stock.current_price, stock.dividend_rate, stock.eps)
 
 # TODO il growth rate lo imposta l'utente? Come calcolarlo in alternativa?
