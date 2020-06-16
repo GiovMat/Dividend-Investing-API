37abc06157703a2a8a59a5eaa61924362c7f77a9
GiovMat <giovmat@gmail.com>
2020-06-16 13:12:46 +0200

    Aggiunto flask-cors

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import dividend_eng as de
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/list', methods=['POST'])
 def list():
